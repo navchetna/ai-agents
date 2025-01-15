3f3be19990f14ab7f3f1de14557f74f070272ee5
Pranav Singh <pranav.singh@intel.com>
2025-01-15 04:50:07 -0500

    add support for retriever
    Signed-off-by: Pranav Singh <pranav.singh@intel.com>

--- comps/__init__.py
+++ comps/__init__.py
@@ -1,6 +1,14 @@
 # comps package
 
-from comps.parsers.treeparser import TreeParser
 from comps.core.logger import CustomLogger
-from comps.proto.docarray import DocPath
+from comps.proto.docarray import (
+  DocPath,
+  EmbedDoc,
+    EmbedMultimodalDoc,
+    SearchedDoc,
+    SearchedMultimodalDoc,
+    TextDoc,
+)
 from comps.core.microservice import opea_microservices, register_microservice
+from comps.core.constants import MegaServiceEndpoint, ServiceRoleType, ServiceType
+from comps.core.base_statistics import statistics_dict, register_statistics
--- comps/proto/docarray.py
+++ comps/proto/docarray.py
@@ -25,3 +25,38 @@ class DocPath(BaseDoc):
     process_table: bool = False
     table_strategy: str = "fast"
 
+
+class EmbedDoc(BaseDoc):
+    text: Union[str, List[str]]
+    embedding: Union[conlist(float, min_length=0), List[conlist(float, min_length=0)]]
+    search_type: str = "similarity"
+    k: int = 4
+    distance_threshold: Optional[float] = None
+    fetch_k: int = 20
+    lambda_mult: float = 0.5
+    score_threshold: float = 0.2
+    constraints: Optional[Union[Dict[str, Any], List[Dict[str, Any]], None]] = None
+
+
+class EmbedMultimodalDoc(EmbedDoc):
+    # extend EmbedDoc with these attributes
+    url: Optional[ImageUrl] = Field(
+        description="The path to the image. It can be remote (Web) URL, or a local file path.",
+        default=None,
+    )
+    base64_image: Optional[str] = Field(
+        description="The base64-based encoding of the image.",
+        default=None,
+    )
+
+class SearchedDoc(BaseDoc):
+    retrieved_docs: DocList[TextDoc]
+    initial_query: str
+    top_n: int = 1
+
+    class Config:
+        json_encoders = {np.ndarray: lambda x: x.tolist()}
+
+
+class SearchedMultimodalDoc(SearchedDoc):
+    metadata: List[Dict[str, Any]]
