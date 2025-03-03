2082fe3efd2dc58f2dd6fcd081de5a90ef1baf21
Pranav Singh <pranav.singh@intel.com>
2025-03-03 13:36:25 +0530

    update rag support for student companion
    Signed-off-by: Pranav Singh <pranav.singh@intel.com>

--- design-patterns/student-companion/lib/utils.ts
+++ design-patterns/student-companion/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// export const DATA_PREP_URL = process.env.NEXT_PUBLIC_DATA_PREP_URL as string;
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL as string;
+export const DB_NAME = "student-companion";
