e3db7ee981e3e801d6cd2fdd168cd77528c4207c
Ervin <ervincas13@gmail.com>
2025-10-15 06:55:18 -0700

    Update env example

--- install/docker/research-assistant/env-example.sh
+++ install/docker/research-assistant/env-example.sh
@@ -1,5 +1,7 @@
 export no_proxy="127.0.0.1,localhost,dataprep-redis,tei-embedding-service,retriever,tei-reranking-service,backend,mongodb,vllm-service,whisper-service,groq-service"
 
+export LOGFLAG=True
+
 export EMBEDDING_MODEL_ID="BAAI/bge-small-en-v1.5"
 export RERANK_MODEL_ID="BAAI/bge-reranker-base"
 export LLM_MODEL_ID="meta-llama/Llama-3.2-1B-Instruct"
@@ -20,12 +22,15 @@ export MEGA_SERVICE_PORT=5008
 export SERVER_HOST_URL="localhost:$MEGA_SERVICE_PORT"
 export NEXT_PUBLIC_SERVER_URL=$SERVER_HOST_URL
 
-export HUGGINGFACEHUB_API_TOKEN=
-export DATAPREP_OUT_DIR=/home/ervin/ai-agents/comps/dataprep # path to store processed data
-export HF_CACHE=/home/ervin/.cache # path to cache dir
-export GROQ_API_KEY=
-export LOGFLAG=True
-
 export MONGO_HOST=mongodb
 export MONGO_USERNAME=agents
 export MONGO_PASSWORD=agents
+
+# User specific environment variables
+
+export HUGGINGFACEHUB_API_TOKEN=
+export GROQ_API_KEY=
+
+export DATAPREP_OUT_DIR=/home/ecasteli/ai-agents/comps/dataprep # path to store processed data
+export HF_CACHE=/home/ecasteli/.cache/huggingface/hub # path to huggingface cache dir
+export DATAPREP_CACHE=/home/ecasteli/.cache/ # path to cache dir
